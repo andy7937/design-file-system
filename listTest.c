232a29d352acecc87188ba5d0c1d646cab0ed762
keehoon <lee.andy2004@gmail.com>
2024-05-05 15:45:49 +1200

    save2

--- listTest.c
+++ listTest.c
@@ -4,23 +4,14 @@
 
 int main(void) {
 
-    format("TestFS");
-    create("/dir1/fd.txt");
-    create("/dir1/fi.txt");
-    create("/fi.txt");
-    create("/dir2");
-    // file2 remains empty
-    char result[1024] = {0}; // Buffer to hold the result from the list function
+    char data[128];
+    list(data, "/");
+    printf("Data read from /: %s\n", data);
+    list(data, "/Dir1");
+    printf("Data read from /Dir1: %s\n", data);
+    list(data, "/Dir1/Dir2");
+    printf("Data read from /Dir1/Dir2: %s\n", data);
 
-    // Test 1: Listing a directory with files
-    list(result, "/dir1");
-    printf("Test 1: List dir1\n%s\n", result);
-    
-    list(result, "/dir2");
-    printf("Test 2: List dir2\n%s\n", result);
-
-    list(result, "/");
-    printf("Test 3: List root\n%s\n", result);
 
     return EXIT_SUCCESS;
 }
